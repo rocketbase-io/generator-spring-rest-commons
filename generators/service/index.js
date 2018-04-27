5a27a4f9c21ce2c063ad93170257c335440350fc
Marten Prieß <marten@rocketbase.io>
2018-04-27 13:15:00 +0200

    added feature to read out pom - reduce questions for service - more flexible to name api and server module

--- generators/service/index.js
+++ generators/service/index.js
@@ -2,8 +2,9 @@
 const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
-const fs = require('fs')
 const _ = require('lodash')
+const fs = require('fs')
+const parseString = require('xml2js').parseString
 
 module.exports = class extends Generator {
 
@@ -11,10 +12,13 @@ module.exports = class extends Generator {
     super(args, opts)
     this.props = {fields: []}
     this.sayYo = _.has(opts, 'subCall')
+    this.apiPathName = null
+    this.serverPathName = null
+    this.springData = null
   }
 
   initializing () {
-    this.placeholder = {name: ''}
+    this.placeholder = {name: '', packageName: ''}
   }
 
   prompting () {
@@ -25,7 +29,51 @@ module.exports = class extends Generator {
       ))
     }
 
+    // detect modules
+    try {
+      if (fs.existsSync(this.destinationPath('pom.xml'))) {
+        let pomContent = fs.readFileSync(this.destinationPath('pom.xml'), 'utf8')
+        parseString(pomContent, (err, result) => {
+          this.xmlJson = result
+          let modules = result.project.modules[0].module
+          try {
+            this.placeholder.packageName = result.project.groupId
+          } catch (e) {
+          }
+          if (modules.length === 2) {
+            let apiModules = modules.filter(m => (/\-api/gi).test(m))
+            if (apiModules.length === 1) {
+              this.apiPathName = apiModules[0]
+              this.serverPathName = modules.filter(m => m !== this.apiPathName)
+            } else {
+              let serverModules = modules.filter(m => (/\-(server|rest)/gi).test(m))
+              if (serverModules.length === 1) {
+                this.serverPathName = serverModules[0]
+                this.apiPathName = modules.filter(m => m !== this.serverPathName)
+              }
+            }
+
+            if (this.serverPathName !== null && fs.existsSync(this.destinationPath(this.serverPathName + '/pom.xml'))) {
+              let pomServerContent = fs.readFileSync(this.destinationPath(this.serverPathName + '/pom.xml'), 'utf8')
+              parseString(pomServerContent, (errServer, resultServer) => {
+                let dependencies = resultServer.project.dependencies[0].dependency
+                if (dependencies.filter(d => (/\-mongodb/gi).test(d.artifactId)).length >= 1) {
+                  this.springData = 'mongodb'
+                } else if (dependencies.filter(d => (/\-jpa/gi).test(d.artifactId)).length >= 1) {
+                  this.springData = 'jpa'
+                }
+              })
+            }
+
+          }
+        })
+      }
+    } catch (e) {
+      console.error('couldn\'t parse pom.xml')
+    }
+
     const prompts = [{
+      when: () => {return this.apiPathName === null},
       type: 'input',
       name: 'projectName',
       message: 'Your project name (needs to be same with project-generator!)',
@@ -38,10 +86,15 @@ module.exports = class extends Generator {
           return chalk.red('The projectName name cannot be empty')
         }
         this.placeholder.projectName = input
+
+        this.apiPathName = input + '-api'
+        this.serverPathName = input + '-server'
+
         return true
       },
       store: true
     }, {
+      default: () => {return this.placeholder.packageName},
       type: 'input',
       name: 'packageName',
       message: 'Base code package structure of project (needs to be same with project-generator!)',
@@ -56,6 +109,7 @@ module.exports = class extends Generator {
       },
       store: true
     }, {
+      when: () => {return this.springData === null},
       type: 'list',
       name: 'springData',
       message: 'Which Spring-Data version?',
@@ -95,12 +149,12 @@ module.exports = class extends Generator {
         // To access props later use this.props.someAnswer;
         this.props = _.assign(answers, this.props)
 
-        this.props.mongoDb = answers.springData === 'mongodb'
+        this.props.mongoDb = (this.springData !== null ? this.springData : answers.springData) === 'mongodb'
         this.props.idClass = this.props.mongoDb ? 'String' : 'Long'
 
         this.props.entityCamelCase = _.lowerFirst(answers.entityName)
         this.props.entityKebabCase = _.kebabCase(answers.entityName)
-        this.props.entitySnakeCaseUpper = _.kebabCase(answers.entityName).replace('-','_').toUpperCase()
+        this.props.entitySnakeCaseUpper = _.kebabCase(answers.entityName).replace('-', '_').toUpperCase()
 
         this.props.entityFolder = answers.entityName.toLowerCase()
         // some transformations
@@ -116,15 +170,15 @@ module.exports = class extends Generator {
     var dPath = this.destinationPath.bind(this)
 
     // api
-    copyTpl(tPath('api/java/package/dto/entity/_EntityRead.java'), dPath(props.projectName + '-api/src/main/java/' + props.basePath + '/dto/' + props.entityFolder + '/' + props.entityName + 'Read.java'), props)
-    copyTpl(tPath('api/java/package/dto/entity/_EntityWrite.java'), dPath(props.projectName + '-api/src/main/java/' + props.basePath + '/dto/' + props.entityFolder + '/' + props.entityName + 'Write.java'), props)
-    copyTpl(tPath('api/java/package/resource/_EntityResource.java'), dPath(props.projectName + '-api/src/main/java/' + props.basePath + '/resource/' + props.entityName + 'Resource.java'), props)
+    copyTpl(tPath('api/java/package/dto/entity/_EntityRead.java'), dPath(this.apiPathName + '/src/main/java/' + props.basePath + '/dto/' + props.entityFolder + '/' + props.entityName + 'Read.java'), props)
+    copyTpl(tPath('api/java/package/dto/entity/_EntityWrite.java'), dPath(this.apiPathName + '/src/main/java/' + props.basePath + '/dto/' + props.entityFolder + '/' + props.entityName + 'Write.java'), props)
+    copyTpl(tPath('api/java/package/resource/_EntityResource.java'), dPath(this.apiPathName + '/src/main/java/' + props.basePath + '/resource/' + props.entityName + 'Resource.java'), props)
 
     // server
-    copyTpl(tPath('server/java/package/controller/_EntityController.java'), dPath(props.projectName + '-server/src/main/java/' + props.basePath + '/controller/' + props.entityName + 'Controller.java'), props)
-    copyTpl(tPath('server/java/package/converter/_EntityConverter.java'), dPath(props.projectName + '-server/src/main/java/' + props.basePath + '/converter/' + props.entityName + 'Converter.java'), props)
-    copyTpl(tPath('server/java/package/model/_EntityEntity.java'), dPath(props.projectName + '-server/src/main/java/' + props.basePath + '/model/' + props.entityName + 'Entity.java'), props)
-    copyTpl(tPath('server/java/package/repository/_EntityRepository.java'), dPath(props.projectName + '-server/src/main/java/' + props.basePath + '/repository/' + props.entityName + 'Repository.java'), props)
+    copyTpl(tPath('server/java/package/controller/_EntityController.java'), dPath(this.serverPathName + '/src/main/java/' + props.basePath + '/controller/' + props.entityName + 'Controller.java'), props)
+    copyTpl(tPath('server/java/package/converter/_EntityConverter.java'), dPath(this.serverPathName + '/src/main/java/' + props.basePath + '/converter/' + props.entityName + 'Converter.java'), props)
+    copyTpl(tPath('server/java/package/model/_EntityEntity.java'), dPath(this.serverPathName + '/src/main/java/' + props.basePath + '/model/' + props.entityName + 'Entity.java'), props)
+    copyTpl(tPath('server/java/package/repository/_EntityRepository.java'), dPath(this.serverPathName + '/src/main/java/' + props.basePath + '/repository/' + props.entityName + 'Repository.java'), props)
   }
 
   install () {

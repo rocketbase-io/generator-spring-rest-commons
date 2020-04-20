5c5e20bd832a92021922d599010772c115482fad
Marten <marten@rocketbase.io>
2020-04-20 07:50:26 +0200

    fix: corrected entity name

--- generators/service/templates/model/java/package/repository/_EntityRepository.java
+++ generators/service/templates/model/java/package/repository/_EntityRepository.java
@@ -4,9 +4,10 @@ import <%= packageName %>.model.<%= entityName %>Entity;
 <%_ if (mongoDb) { _%>
 import org.springframework.data.mongodb.repository.MongoRepository;
 <%_ } else { _%>
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 <%_ } _%>
 
-public interface <%= entityName %>Repository extends <% if (mongoDb) { %>MongoRepository<% } else { %>PagingAndSortingRepository<% } %><<%= entityName %>Entity, <%= idClass %>> {
+public interface <%= entityName %>Repository extends <% if (mongoDb) { %>MongoRepository<% } else { %>PagingAndSortingRepository<% } %><<%= entityName %>Entity, <%= idClass %>> <% if (!mongoDb) {%>, JpaSpecificationExecutor<<%= entityName %>Entity><%}%> {
 
 }
